bb38c51474f09c79784e87c1fe1e91fafde682ca
ArpitSharmaa <arpitskvashista@gmail.com>
2023-09-08 21:37:28 +0530

    live chat room socket active 2nd comit

--- src/main/kotlin/com/ReXtOr/Chatroomroutes/livesession/livesessob.kt
+++ src/main/kotlin/com/ReXtOr/Chatroomroutes/livesession/livesessob.kt
@@ -1,6 +1,8 @@
 package com.ReXtOr.Chatroomroutes.livesession
 
+import com.ReXtOr.Database.DatabaseConnectiom.databaseConnection
 import com.ReXtOr.Database.LiveRooms.Utils.liveroomtableImp
+import com.ReXtOr.Database.LiveRooms.model.liveRoomsNames
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
@@ -13,32 +15,39 @@ import java.net.http.WebSocket
 import java.util.concurrent.ConcurrentHashMap
 
 fun Route.livesession(){
-    val room = liveroomtableImp()
-    val scope = CoroutineScope(Dispatchers.IO)
-    val rooms = scope.async {
-        room.getliverooms()
-    }
-
-    scope.launch {
-        val livesessions = rooms.await()
-        val members = ConcurrentHashMap<WebSocketSession,Unit>()
-        for (session in livesessions.listOfRooms){
-            webSocket ("chatroom/${session.nameOfRoom}"){
-                members[this]= Unit
-                this.send("Connected to the room be respectfull")
-                for (frame in this.incoming){
-                    frame as Frame.Text
-                    val recieved = frame.readText()
-                    val others = members.filter {
-                        it.key != this
-                    }
-                    others.forEach{
-                        it.key.send(recieved)
-                    }
+    val roomstable = liveroomtableImp()
+//    val room = liveroomtableImp()
+//    val scope = CoroutineScope(Dispatchers.IO)
+//    val rooms = scope.async {
+//        room.getliverooms()
+//    }
+//
+//    scope.launch {
+//        val livesessions = rooms.await()
+        val members = ConcurrentHashMap<WebSocketSession, Unit>()
+//        for (session in livesessions.listOfRooms){
+        webSocket("chatroom/hi") {
+            val nameofRoom = call.parameters["roomName"]
+            nameofRoom?.let {
+                liveRoomsNames(
+                    nameOfRoom = it
+                )
+            }?.let { roomstable.insertliveroom(it) }
+            members[this] = Unit
+            this.send("Connected to the room be respectful")
+            for (frame in this.incoming) {
+                frame as Frame.Text
+                val recieved = frame.readText()
+                val others = members.filter {
+                    it.key != this
+                }
+                others.forEach {
+                    it.key.send(recieved)
                 }
             }
         }
 
-    }
+//    }
+//    }
 
 }
